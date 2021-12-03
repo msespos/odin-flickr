ba6697e8dcf7ca0d762cd6cfa009b53fb3b16655
Mike Esposito <mikeesposito6852@gmail.com>
2021-12-03 14:17:32 -0800

    Add resources and root

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :static_pages, only: [:index, :create]
+  root to: 'static_pages#index'
 end
