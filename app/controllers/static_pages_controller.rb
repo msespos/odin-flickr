315456d63baa6580d5ced2fbe7a121da47689b6b
Mike Esposito <mikeesposito6852@gmail.com>
2021-12-03 14:17:06 -0800

    Add index and create actions

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,15 @@
+require 'flickr'
+
 class StaticPagesController < ApplicationController
+  def index
+    flickr = Flickr.new ENV['FLICKR_KEY'], ENV['FLICKR_SECRET']
+    @user_id = session[:user_id]
+    session[:user_id] = nil
+    @photos = flickr.photos.search(user_id: @user_id)
+  end
+
+  def create
+    session[:user_id] = params[:user_id]
+    redirect_to root_path
+  end
 end
