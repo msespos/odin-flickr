403ff056866867f0e792d2cf44bbaf06b7d38886
Mike Esposito <mikeesposito6852@gmail.com>
2021-12-05 08:08:41 -0800

    Fix error on blank responses and add Easter egg for blank responses

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,9 +2,8 @@ require 'flickr'
 
 class StaticPagesController < ApplicationController
   def index
+    @user_id = user_id
     flickr = Flickr.new ENV['FLICKR_KEY'], ENV['FLICKR_SECRET']
-    @user_id = session[:user_id]
-    session[:user_id] = nil
     response = flickr.photos.search(user_id: @user_id)
     @photos = response.map do |p|
       "https://farm#{p.farm}.staticflickr.com/#{p.server}/#{p.id}_#{p.secret}.jpg"
@@ -15,4 +14,14 @@ class StaticPagesController < ApplicationController
     session[:user_id] = params[:user_id]
     redirect_to root_path
   end
+
+  private
+
+    def user_id
+      user_id = if session[:user_id] == ""
+                  "otter"
+                else
+                  session[:user_id]
+                end
+    end
 end
