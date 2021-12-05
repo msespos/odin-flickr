ee59e4f9fffd90850ec9b6ee3e59dfa1d645535d
Mike Esposito <mikeesposito6852@gmail.com>
2021-12-05 11:49:02 -0800

    Move @tags line into index

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,8 +5,8 @@ require 'flickr'
 # Static Pages Controller class
 class StaticPagesController < ApplicationController
   def index
-    @tags = tags
     flickr = Flickr.new ENV['FLICKR_KEY'], ENV['FLICKR_SECRET']
+    @tags = session[:tags] == '' ? 'otter' : session[:tags]
     response = flickr.photos.search(tags: @tags)
     @photos = response.map do |p|
       "https://farm#{p.farm}.staticflickr.com/#{p.server}/#{p.id}_#{p.secret}.jpg"
@@ -16,11 +16,5 @@ class StaticPagesController < ApplicationController
   def create
     session[:tags] = params[:tags]
     redirect_to root_path
-  end
-
-  private
-
-  def tags
-    session[:tags] == '' ? 'otter' : session[:tags]
   end
 end
